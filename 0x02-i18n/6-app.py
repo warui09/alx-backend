320eec07234b55026c073ace49090c63290a9fe4
Kevin Warui <waruimacharia@gmail.com>
2024-04-09 22:28:25 +0300

    Add 6-app.py and 6-index.html

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""basic Flask app"""
+
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, _
+from typing import Dict, Optional
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """config class for app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user() -> Optional[Dict]:
+    """get user if user exists"""
+
+    user_id = request.args.get("login_as")
+
+    if user_id:
+        return users.get(int(user_id))
+
+    return None
+
+
+@app.before_request
+def before_request():
+    """set user as a global variable"""
+
+    g.user = get_user()
+    print(g.user)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get language that best matches supported ones"""
+
+    language = request.args.get("locale")
+    if language and language in Config.LANGUAGES:
+        return language
+
+    if g.user and g.user.locale in Config.LANGUAGES:
+        return g.user.locale
+
+    header_language = request.accept_languages.best_match(Config.LANGUAGES)
+    if header_language:
+        return header_language
+
+    return Config.BABEL.DEFAULT.LANGUAGE
+
+
+@app.route("/")
+def index() -> str:
+    """render the index page"""
+
+    return render_template(
+        "5-index.html",
+        title=_("home_title"),
+        header=_("home_header"),
+    )
+
+
+if __name__ == "__main__":
+    app.run()
