56f78ee0ff7cc2e8d5b40024a2cc9b4807671aa9
Kevin Warui <waruimacharia@gmail.com>
2024-04-09 16:03:33 +0000

    Add 5-app.py, templates/5-index.html

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,6 +4,13 @@
 from flask import Flask, render_template, g, request
 from flask_babel import Babel, _
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 class Config:
     """config class for app"""
@@ -18,11 +25,25 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+def get_user() -> str:
+    """get user if user exists"""
+
+    user_id = request.args.get("login_as")
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """set user as a global variable"""
+
+    flask.g.user = get_user()
+
+
 @babel.localeselector
 def get_locale() -> str:
     """get language that best matches supported ones"""
 
-    language = request.args.get('locale')
+    language = request.args.get("locale")
     if language and language in Config.LANGUAGES:
         return language
 
@@ -33,7 +54,9 @@ def get_locale() -> str:
 def index() -> str:
     """render the index page"""
 
-    return render_template("4-index.html", title=_('home_title'), header=_('home_header'))
+    return render_template(
+        "5-index.html", title=_("home_title"), header=_("home_header")
+    )
 
 
 if __name__ == "__main__":
