794e3c461edeca08e935c4b458b0a400a47f0caa
Kevin Warui <waruimacharia@gmail.com>
2024-05-07 17:25:00 +0300

    Add 5-subscriber.js and 5-publisher.js

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+const channel = 'holberton school';
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.subscribe(channel);
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
